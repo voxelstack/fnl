ce61e87ec2dee941108227c14dea3027a0981952
voxelstack <voxelstack@gmail.com>
2025-10-20 00:19:39 -0300

    Read symbol and nil.

--- src/lib/fnl.test.ts
+++ src/lib/fnl.test.ts
@@ -11,16 +11,25 @@ describe("evaluate", () => {
     test("boolean", () => {
         expect(evaluate(true)).toStrictEqual(true);
     });
+    test("nil", () => {
+        expect(evaluate(null)).toStrictEqual(null);
+    });
 });
 
 describe("read", () => {
+    test("symbol", () => {
+        expect(read(`symbol`)).toMatchObject({ name: "symbol" })
+    });
     test("number", () => {
-        expect(evaluate(read(`22`))).toStrictEqual(22);
+        expect(read(`22`)).toStrictEqual(22);
     });
     test("string", () => {
-        expect(evaluate(read(`"fnl"`))).toStrictEqual("fnl");
+        expect(read(`"fnl"`)).toStrictEqual("fnl");
     });
     test("boolean", () => {
-        expect(evaluate(read(`true`))).toStrictEqual(true);
+        expect(read(`true`)).toStrictEqual(true);
+    });
+    test("nil", () => {
+        expect(read(`nil`)).toStrictEqual(null);
     });
 });
--- src/lib/fnl.ts
+++ src/lib/fnl.ts
@@ -1,13 +1,19 @@
-class Symbol {}
-type Atom = Symbol | number | string | boolean;
-type Cons = Atom[];
-type Object = Atom | Cons;
+class Symbol {
+    public readonly name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+type Atom = Symbol | number | string | boolean | null;
+type List = Atom[];
+type Object = Atom | List;
 
 export function evaluate(exp: Object) {
     if (atom(exp)) {
         if (symbol(exp)) {
             throw new Error("Unimplemented.");
-        } else if (number(exp) || string(exp) || boolean(exp)) {
+        } else if (number(exp) || string(exp) || boolean(exp) || nil(exp)) {
             return exp;
         } else {
             throw new Error("Cannot evaluate.");
@@ -22,21 +28,17 @@ export function read(input: string): Object {
     
     interface Token {
         type:
+            | "symbol"
             | "number"
             | "string"
             | "boolean"
+            | "nil"
         ;
         value: string;
         span: [number, number];
     }
 
     function tokenize(input: string) {
-        // https://www.lispworks.com/documentation/HyperSpec/Body/02_ac.htm
-        const whitespace = (c: string) => (c === "\n" || c == " ");
-        const latin = (c: string) => (c >= "a" && c <= "z") || (c >= "A" && c <= "Z");
-        const numeric = (c: string) => (c >= "0" && c <= "9");
-        const special = (c: string) => "!$\"'(),_-./:;?+<=>#%&*@[\\]{|}`^~".includes(c);
-
         const tokens: Token[] = [];
         
         for (let i = 0; i < input.length; ++i) {
@@ -45,54 +47,72 @@ export function read(input: string): Object {
                 continue;
             }
 
+            let len: number;
+            let token: Token;
             if (next === "\"") {
-                takeString();
+                len = readWhile((c) => c !== "\"", 1);
+                token = produce("string", len, (s) => s.slice(1));
             } else if (numeric(next) || (next === "-" && numeric(input[i + 1]))) {
-                takeNumber();
-            } else if ((next === "t" || next === "f") && tryBoolean()) {
-                takeBoolean();
-            } else {
-                throw new Error("Cannot read.");
-            }
-
-            function emit(type: Token["type"], len: number, parse: (value: string) => any) {
-                tokens.push({
-                    type,
-                    value: parse(input.slice(i, i + len)),
-                    span: [i, i + len]
-                });
-                i += len;
-            }
-            function takeString() {
-                let len = 1;
-                while (i + len < input.length && input[i + len] !== "\"") ++len;
-                emit("string", len, (s) => s.slice(1));
-            }
-            function takeNumber() {
-                let len = next === "-" ? 1 : 0;
-                while (i + len < input.length && numeric(input[i + len])) ++len;
+                len = readWhile(numeric, next === "-" ? 1 : 0);
                 if (input[i + len] === ".") {
                     ++len;
                     if (i + len >= input.length || !numeric(input[i + len])) {
                         throw new Error("Missing decimal part.");
                     }
-                    while (i + len < input.length && numeric(input[i + len])) ++len;
+                    len = readWhile(numeric, len);
                 }
-                emit("number", len, Number);
+                token = produce("number", len, Number);
+            } else if (tryWord("true")) {
+                len = "true".length;
+                token = produce("boolean", len, () => true);
+            } else if (tryWord("false")) {
+                len = "false".length;
+                token = produce("boolean", len, () => false);
+            } else if (tryWord("nil")) {
+                len = "nil".length;
+                token = produce("nil", len, () => null);
+            } else if (valid(next)) {
+                len = readWhile(valid);
+                token = produce("symbol", len, (s) => new Symbol(s));
+            } else {
+                throw new Error("Cannot read.");
             }
-            function tryBoolean() {
-                const word = input[i] === "t" ? "rue" : "alse";
+            i += len;
+            tokens.push(token);
+
+            // https://www.lispworks.com/documentation/HyperSpec/Body/02_ac.htm
+            function whitespace(c: string) {
+                return (c === "\n" || c == " ");
+            }
+            function latin(c: string) {
+                return (c >= "a" && c <= "z") || (c >= "A" && c <= "Z");
+            }
+            function numeric(c: string) {
+                return (c >= "0" && c <= "9");
+            }
+            function special(c: string) {
+                // Omit parenthesis, they are delimiters.
+                return "!$\"',_-./:;?+<=>#%&*@[\\]{|}`^~".includes(c);
+            }
+            function valid(c: string) {
+                return latin(c) || numeric(c) || special(c);
+            }
+            function tryWord(word: string) {
                 for (let j = 0; i + j < input.length && j < word.length; ++j) {
-                    if (word[j] !== input[i + j + 1]) return false;
+                    if (word[j] !== input[i + j]) return false;
                 }
                 return true;
             }
-            function takeBoolean() {
-                if (next === "t") {
-                    emit("boolean", "true".length, () => true);
-                } else {
-                    emit("boolean", "false".length, () => false);
-                }
+            function readWhile(cond: (c: string) => boolean, len = 0) {
+                while (i + len < input.length && cond(input[i + len])) ++len;
+                return len;
+            }
+            function produce(type: Token["type"], len: number, parse: (value: string) => any) {
+                return {
+                    type,
+                    value: parse(input.slice(i, i + len)),
+                    span: [i, i + len]
+                } as Token;
             }
         }
 
@@ -105,9 +125,11 @@ export function read(input: string): Object {
         }
 
         switch (tokens[0].type) {
+            case "symbol":
             case "number":
             case "string":
             case "boolean":
+            case "nil":
                 return tokens[0].value;
         }
     }
@@ -118,3 +140,4 @@ function symbol(o: Object): o is Symbol { return o instanceof Symbol; }
 function number(o: Object): o is number { return typeof o === "number"; }
 function string(o: Object): o is string { return typeof o === "string"; }
 function boolean(o: Object): o is boolean { return typeof o === "boolean"; }
+function nil(o: Object): o is null { return o === null };
