d0458c2cfebd48070c95491acfbd5f1197e42bda
voxelstack <voxelstack@gmail.com>
2025-11-03 01:45:12 -0300

    Number reader.

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,17 @@ use wasm_bindgen::JsValue;
 
 #[derive(Debug)]
 pub enum Object {
-    Nil(()),
     String(String),
+    Number(f64),
+    Nil(()),
 }
 
 impl Into<JsValue> for Object {
     fn into(self) -> JsValue {
         match self {
-            Object::Nil(()) => JsValue::null(),
             Object::String(string) => JsValue::from_str(&string),
+            Object::Number(number) => JsValue::from_f64(number),
+            Object::Nil(()) => JsValue::null(),
         }
     }
 }
@@ -29,28 +31,37 @@ impl fmt::Display for ParseError {
     }
 }
 
-pub async fn read<I>(mut input: Pin<&mut Peekable<I>>) -> Result<Object, ParseError>
+pub async fn read<I>(input: &mut Pin<&mut Peekable<I>>) -> Result<Object, ParseError>
 where
     I: Stream<Item = char>,
 {
     set_panic_hook();
 
-    while let Some(ch) = input.as_mut().next().await {
-        if ch.is_whitespace() {
-            continue;
-        }
+    let object: Result<Object, ParseError> = loop {
+        // We can only peek once, so call next here and pass ch to the reader if needed.
+        match input.as_mut().next().await {
+            Some(ch) if ch.is_whitespace() => continue,
+            Some(ch) => {
+                break match ch {
+                    '"' => read_string(input).await,
+                    '-' if input.as_mut().peek().await.is_some_and(|x| x.is_numeric()) => {
+                        read_number(input, '-').await
+                    }
+                    _ if ch.is_numeric() => read_number(input, ch).await,
+                    _ => todo!(),
+                }
+            }
+            None => break Ok(Object::Nil(())),
+        };
+    };
 
-        return match ch {
-            '"' => read_string(input),
-            _ => todo!(),
-        }
-        .await;
+    match input.as_mut().next().await {
+        Some(_) => Err(ParseError(String::from("Expected EOF"))),
+        None => object,
     }
-
-    Ok(Object::Nil(()))
 }
 
-async fn read_string<I>(mut input: Pin<&mut Peekable<I>>) -> Result<Object, ParseError>
+async fn read_string<I>(input: &mut Pin<&mut Peekable<I>>) -> Result<Object, ParseError>
 where
     I: Stream<Item = char>,
 {
@@ -72,4 +83,33 @@ where
         }
     }
     Err(ParseError(String::from("Unterminated string")))
+}
+
+async fn read_number<I>(input: &mut Pin<&mut Peekable<I>>, curr: char) -> Result<Object, ParseError>
+where
+    I: Stream<Item = char>,
+{
+    let mut number = String::from(curr);
+    while let Some(ch) = input.as_mut().peek().await {
+        match ch {
+            ch if ch.is_numeric() => {
+                number.push(*ch);
+                input.as_mut().next().await;
+            }
+            '.' => {
+                number.push(*ch);
+                input.as_mut().next().await;
+
+                if let Some(ch) = input.as_mut().next_if(|x| x.is_numeric()).await {
+                    number.push(ch);
+                    continue;
+                }
+                return Err(ParseError(String::from("Missing decimal part")));
+            }
+            _ => break,
+        }
+    }
+    Ok(Object::Number(number.parse::<f64>().map_err(|_| {
+        ParseError(String::from("Could not parse number"))
+    })?))
 }
--- tests/read.rs
+++ tests/read.rs
@@ -8,41 +8,38 @@ use wasm_bindgen_test::{wasm_bindgen_test_configure, *};
 
 wasm_bindgen_test_configure!(run_in_browser);
 
-#[wasm_bindgen_test]
-async fn read_simple_string() {
-    let source = r#"
-        "read a simple string"
-    "#;
+macro_rules! test_read {
+    ($object:ident, [ $($name:ident: $source:expr => $expected:expr,)* ]) => {
+        $(
+            #[wasm_bindgen_test]
+            async fn $name() {
+                let mut stream = stream::iter($source.chars()).peekable();
+                let stream = &mut Pin::new(&mut stream);
 
-    let mut stream = stream::iter(source.chars()).peekable();
-    let stream = Pin::new(&mut stream);
+                let received = read(stream).await;
 
-    let received = read(stream).await;
-    let expected = String::from("read a simple string");
-
-    match received {
-        Ok(Object::String(received)) => assert_eq!(received, expected),
-        _ => panic!("expected a string object"),
-    }
+                match received {
+                    Ok(Object::$object(received)) => assert_eq!(received, $expected),
+                    _ => panic!("expected a {} object", stringify!($object)),
+                }
+            }
+        )*
+    };
 }
 
-#[wasm_bindgen_test]
-async fn read_escaped_string() {
-    let source = r#"
-        "read a \\string\\ with\n\"escaped\" characters"
-    "#;
-
-    let mut stream = stream::iter(source.chars()).peekable();
-    let stream = Pin::new(&mut stream);
-
-    let received = read(stream).await;
-    let expected = String::from(
-        r#"read a \string\ with
+test_read!(String, [
+    read_simple_string:
+    r#""read a simple string""# => r#"read a simple string"#,
+    read_escaped_string:
+    r#""read a \\string\\ with\n\"escaped\" characters""# =>
+    r#"read a \string\ with
 "escaped" characters"#,
-    );
+]);
 
-    match received {
-        Ok(Object::String(received)) => assert_eq!(received, expected),
-        _ => panic!("expected a string object"),
-    }
-}
+test_read!(Number, [
+    read_zero: r#"0"# => 0_f64,
+    read_positive_integer: r#"314"# => 314_f64,
+    read_negative_integer: r#"-314"# => -314_f64,
+    read_positive_float: r#"3.14"# => 3.14_f64,
+    read_negative_float: r#"-3.14"# => -3.14_f64,
+]);
